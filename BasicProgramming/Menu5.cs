aad80c87d63d1734b4e511ee20394d32685ba55a
ilprasetyo <ilprasetyo2125@gmail.com>
2021-04-15 13:43:00 +0700

    Create Menu5(overloading)

---
+++ BasicProgramming/Menu5.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicProgramming
+{
+    class Menu5
+    {
+        static int KuadratMethod(int x)
+        {
+            return x * x;
+        }
+
+        static double KuadratMethod(double x)
+        {
+            return x * x;
+        }
+
+        public void Kuadrat()
+        {
+            Console.WriteLine("Input Integer Number ");
+            int angka1 = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine("Input Decimal Number ");
+            double angka2 = Convert.ToDouble(Console.ReadLine());
+
+            int num1 = KuadratMethod(angka1);
+            double num2 = KuadratMethod(angka2);
+
+            Console.WriteLine("Square Result Integer  "+num1);
+            Console.WriteLine("Square Result Decimal "+num2);
+        }
+    }
+}
